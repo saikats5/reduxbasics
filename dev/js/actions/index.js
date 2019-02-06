50dea9425dd018dcb4b03542d4a58fbd9c86bf39
Saikat <saikats_5@yahoo.co.in>
2019-02-06 07:35:53 +0530

    reflux setup

--- dev/js/actions/index.js
+++
@@ -1,7 +0,0 @@
-export const selectUser = (user) => {
-    console.log("Your clicked: ",user.first);
-    return {
-        type: "USER_SELECTED",
-        payload: user
-    }
-}
--- dev/js/components/app.js
+++ dev/js/components/app.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import UserList from '../containers/user-list';
-import UserDetail from '../containers/user-detail';
-require('../../scss/style.scss');
 
-const App = () => (
-    <div>
-        <h2>Username List:</h2>
-        <UserList />
-        <hr/>
-        <h2>User Details:</h2>
-        <UserDetail />
-    </div>
-);
+class App extends React.Component{
+    render(){
+        return (
+            <div>Testing</div>
+        )
+    }
+}
 
 export default App;
